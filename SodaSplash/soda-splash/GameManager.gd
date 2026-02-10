f8bcede3b57c1fe92bf62b46bc01843730cd31b9
chaseman2505 <chaseman2505@gmail.com>
2026-02-10 01:00:46 -0500

    Added some basic mechanics

--- SodaSplash/soda-splash/GameManager.gd
+++ SodaSplash/soda-splash/GameManager.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-@onready var static_body = get_node("StaticBody2D4")
+@onready var textureProgressBar = get_node("TextureProgressBar")
+var finishedPouring = false
+var minLevel = 20
+var score = 0
+var cups = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,17 +13,36 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		#print("Left mouse held")
-	pass
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and finishedPouring == false:
+		textureProgressBar.value += 0.1
+		if textureProgressBar.value > 100:
+			finishedPouring = true
+			print("You lose!")
+			score = 0
+			cups = 0
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				#static_body.scale.x = 30
-				static_body.position = Vector2(-500, 500)
-				#print("Left mouse button pressed")
+				pass
+			#when left click is released
 			else:
-				#static_body.scale.x = 50
-				static_body.position = Vector2(-134, 53)
+				if finishedPouring == true:
+					textureProgressBar.value = 0
+					finishedPouring = false
+				else:
+					finishedPouring = true
+					if textureProgressBar.value <= 100 and textureProgressBar.value >= minLevel:
+						cups += 1
+						score += textureProgressBar.value
+						print(cups)
+						print(score)
+						if minLevel < 95:
+							minLevel += 5
+						elif minLevel < 99:
+							minLevel += 1
+					else:
+						print("You lose!")
+						score = 0
+						cups = 0
