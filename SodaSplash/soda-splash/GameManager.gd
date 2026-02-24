5f9924d46f4a8cc6bd0db1dddad101ddc5602953
chaseman2505 <chaseman2505@gmail.com>
2026-02-23 21:38:20 -0500

    Pour Delay WIP

--- SodaSplash/soda-splash/GameManager.gd
+++ SodaSplash/soda-splash/GameManager.gd
@@ -5,6 +5,7 @@ extends Node2D
 @onready var fillIndicatorTop = get_node("Cup/FillIndicatorTop")
 @onready var fillIndicatorBottom = get_node("Cup/FillIndicatorTop/FillIndicatorBottom")
 @onready var conveyorBelt = get_node("ConveyorBelt")
+@onready var sodaFountain = get_node("SodaFountain")
 
 @onready var loseSound = get_node("Sounds/LoseSound")
 @onready var resetSound = get_node("Sounds/ResetSound")
@@ -20,47 +21,48 @@ const MINLEVELSTART = 20
 var minLevel = MINLEVELSTART
 var score = 0
 var cups = 0
-var pourDelay = 1
+
+#how long it takes the liquid to fall from the fountain
+#to the surface of the liquid or the bottom of the cup
+var pourDelay = 0.0
+var pourTimer = 0.0
+
+#if left click is done being held or not
 var finishedPouring = false
 var resetAnimation = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pourDelay = CalculatePourDelay()
+	print(pourDelay)
 	Reset()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:	
+func _process(delta: float) -> void:
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and finishedPouring == false and cup.value <= 100 and resetAnimation == false:
-		cup.value += 50 * delta
-		pourSound.play()
-		scoreLabel.text = "Total Score: " + str(int(score)) + " + " + str(int(cup.value)) + "\nRound Score: " + str(int(cup.value)) + "\nCups: " + str(cups)
-		if cup.value > 100:
-			Lose()
-			overflowSound.play()
-			
+		pourTimer += delta
+		#pourSound1.play()
+		if pourTimer >= pourDelay:
+			AddLiquid(delta)
+				
+	if !Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and finishedPouring == true and cup.value <= 100:
+		pourTimer += delta
+		pourDelay = CalculatePourDelay()
+		if pourTimer < pourDelay:
+			AddLiquid(delta)
+		else:
+			print(pourDelay)
+			finishedPouring = true
+			MeasureFill()
+	
+	scoreLabel.text = str(pourTimer)
 #	cup.position.x = move_toward(cup.position.x, 520.8, 640 * delta * 2)
 #	print(cup.position.x)
 
 	if resetAnimation == true:
-		#if the cup is in the screen center
-		if is_equal_approx(cup.position.x, 520.8):
-			conveyorBelt.texture.pause = true
-			fillIndicatorTop.visible = true
-			fillIndicatorTop.value = move_toward(fillIndicatorTop.value, 100 - minLevel, 200 * delta)
-			fillIndicatorBottom.position.y = (4.35 * fillIndicatorTop.value) - 210
-			if fillIndicatorTop.value == 100 - minLevel:
-				resetAnimation = false
-		#if the cup is to the left of the screen center
-		elif cup.position.x < 520.8:
-			cup.position.x = move_toward(cup.position.x, -119.2, 640 * delta * 2)
-			if is_equal_approx(cup.position.x, -119.2):
-				cup.position.x = 1160.8
-		#if the cup is to the right of the screen center
-		else:
-			cup.position.x = move_toward(cup.position.x, 520.8, 640 * delta * 2)
-			
+		ResetAnimation(delta)
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -70,8 +72,15 @@ func _input(event):
 				if finishedPouring == true:
 					Reset()
 				else:
-					MeasureFill()
-					
+					pourTimer = 0
+
+func AddLiquid(delta):
+	#pourSound2.play()
+	cup.value += 50 * delta
+	scoreLabel.text = "Total Score: " + str(int(score)) + " + " + str(int(cup.value)) + "\nRound Score: " + str(int(cup.value)) + "\nCups: " + str(cups)
+	if cup.value > 100:
+		Lose()
+		overflowSound.play()
 
 func Lose():
 	loseSound.play()
@@ -86,15 +95,16 @@ func Reset():
 	cup.value = 3
 	scoreLabel.text = "Total Score: " + str(int(score)) + "\nRound Score: 0" + "\nCups: " + str(cups)
 	fillIndicatorTop.value = 0
-	finishedPouring = false
 	resetAnimation = true
 	cup.position.x -= 0.01
 	conveyorBelt.texture.pause = false
 	fillIndicatorTop.visible = false
+	pourDelay = CalculatePourDelay()
+	print(pourDelay)
+	
 	
 	
 func MeasureFill():
-	finishedPouring = true
 	#if you didn't pour enough to go to the next lexel
 	if cup.value < minLevel:
 		Lose()
@@ -117,3 +127,26 @@ func MeasureFill():
 			scoreLabel.text = "Total Score: " + str(int(score)) + "\nRound Score: " + str(int(cup.value) + GOODBONUS) + "\nCups: " + str(cups) + "\nGood +" + str(GOODBONUS)
 		else:
 			scoreLabel.text = "Total Score: " + str(int(score)) + "\nRound Score: " + str(int(cup.value)) + "\nCups: " + str(cups) + "\nOkay"
+
+func ResetAnimation(delta):
+	#if the cup is in the screen center
+	if is_equal_approx(cup.position.x, 520.8):
+		conveyorBelt.texture.pause = true
+		fillIndicatorTop.visible = true
+		fillIndicatorTop.value = move_toward(fillIndicatorTop.value, 100 - minLevel, 200 * delta)
+		fillIndicatorBottom.position.y = (4.35 * fillIndicatorTop.value) - 210
+		if fillIndicatorTop.value == 100 - minLevel:
+			resetAnimation = false
+			pourTimer = 0
+			finishedPouring = false
+	#if the cup is to the left of the screen center
+	elif cup.position.x < 520.8:
+		cup.position.x = move_toward(cup.position.x, -119.2, 640 * delta * 2)
+		if is_equal_approx(cup.position.x, -119.2):
+			cup.position.x = 1160.8
+	#if the cup is to the right of the screen center
+	else:
+		cup.position.x = move_toward(cup.position.x, 520.8, 640 * delta * 2)
+		
+func CalculatePourDelay():
+	return ((cup.size.y * 0.4 * (100 - cup.value) * 0.01) + (cup.position.y - (sodaFountain.position.y + (sodaFountain.texture.get_size().y * 0.4 * 0.5))))/195.072
