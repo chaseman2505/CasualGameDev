3cbb62f3fbdbd364bea18477b8505aec68651321
cjj1516 <cjj1516@g.rit.edu>
2026-02-27 14:47:28 -0500

    Implemented Basic particle scripts

--- SodaSplash/soda-splash/over_flow.gd
+++ SodaSplash/soda-splash/over_flow.gd
@@ -1,4 +1,9 @@
 extends GPUParticles2D
+@export var fill_indicator: TextureProgressBar
+@export var offset_y: float = 0.0
+var isOverflow = false
+
+
 
 
 
@@ -9,4 +14,17 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if fill_indicator.value > 100:
+		lifetime = 1.95
+	else:
+		lifetime = 0.2
+		
+		
+		
+	var fill_ratio = fill_indicator.value/ fill_indicator.max_value
+	var bar_height = fill_indicator.size.y
+	var fill_line_y = -(bar_height * fill_ratio/2.5)
+	
+	var bar_top = fill_indicator.global_position
+	global_position.y = bar_top.y*1.6 + fill_line_y
+	
