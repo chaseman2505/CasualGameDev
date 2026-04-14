47d4f3948501d9b512e399233e80fa2db192a30d
Owen Beck <112406398+oab1881@users.noreply.github.com>
2026-04-14 16:43:16 -0400

    Added New Buttons to home screen

--- Sunshine/sunshine/Scripts/lvl_btn.gd
+++ Sunshine/sunshine/Scripts/lvl_btn.gd
@@ -1,27 +1,54 @@
 extends Button
 
-
+#store original scale
 var original_size := scale
+#size button grows to when hovered over
 var grow_size := Vector2(1.1,1.1)
 
+#Path to level file
 @export_file(".txt") var level_path: String
 @export var board: Node2D
 
+#Default icon texture
+@export var normal_texture: Texture2D
+# Hover (highlighted) icon texture
+@export var hover_texture: Texture2D
 
+#The level number to show on the button
+@export var level_number: int = 1
+#Reference to the Label node that will display the number
+@onready var level_label: Label = $LevelLabel
 
+# Called when the node enters the scene tree
+func _ready() -> void:
+ # Set the default texture
+ if normal_texture:
+  self.icon = normal_texture
+ # Set the level number text
+ if level_label:
+  level_label.text = str(level_number)
+ else:
+  print("LevelLabel not found on ", name)
 
-# Called when the node enters the scene tree for the first time.
+#Called when the node enters the scene tree for the first time.
 func grow_btn(end_size: Vector2, duration: float) -> void:
  var tween := create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
  tween.tween_property(self, 'scale', end_size, duration)
 
 
 func _on_mouse_entered() -> void:
+ # Grow the button slightly
  grow_btn(grow_size, .1)
+ # Swap to the hover texture if assigned
+ if hover_texture:
+  self.icon = hover_texture
 
 
 func _on_mouse_exited() -> void:
  grow_btn(original_size, .1)
+# Swap to the hover texture if assigned
+ if hover_texture:
+  self.icon = normal_texture
 
 
 func _on_pressed() -> void:
--- Sunshine/sunshine/Scripts/menuBtn.gd
+++ Sunshine/sunshine/Scripts/menuBtn.gd
@@ -1,17 +1,32 @@
 extends Button
 
-
+#store button's original scale
 var original_size := scale
+#size that button will grow to when hovered over
 var grow_size := Vector2(.4,.4)
 
+# Default (normal) button texture
+@export var normal_texture: Texture2D
+
+# Highlighted texture when hovering
+@export var hover_texture: Texture2D
+
+#Smoothly scale button size over time
 func grow_btn(end_size: Vector2, duration: float) -> void:
  var tween := create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
  tween.tween_property(self, 'scale', end_size, duration)
 
 
 func _on_mouse_entered() -> void:
+# Grow the button slightly
  grow_btn(grow_size, .1)
+# Swap to the hover texture if assigned
+ if hover_texture:
+  self.icon = hover_texture
 
 
 func _on_mouse_exited() -> void:
  grow_btn(original_size, .1)
+# Swap to the hover texture if assigned
+ if hover_texture:
+  self.icon = normal_texture
